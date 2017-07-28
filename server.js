d4715d12a2c6f565db7fe69ca558f3a194ca661b
yasar-salim <yasarsalim@bytekat.com>
2017-07-28 19:13:11 +0530

    port changed to 3001

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 3000;
+  port = process.env.PORT || 3001;
 
 var mysql = require('mysql');
 var con = mysql.createConnection({
