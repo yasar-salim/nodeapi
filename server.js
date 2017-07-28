b215661427c02dba33414ca8a901ac076c501103
yasar-salim <yasarsalim@bytekat.com>
2017-07-28 15:57:03 +0530

    v1.0.0

---
+++ server.js
@@ -0,0 +1,39 @@
+var express = require('express'),
+  app = express(),
+  port = process.env.PORT || 3000;
+
+var mysql = require('mysql');
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "hotel"
+});
+
+app.listen(port);
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+});
+
+app.get('/categories',function(req,res){
+    
+        con.query("SELECT * FROM categories", function (err, result, fields) {
+            if (err) throw err;
+            res.send(fil);
+        });
+
+});
+
+app.get('/subcategories/:id',function(req,res){
+    var id=req.params.id;
+    
+        con.query("SELECT * FROM sub WHERE cid="+id, function (err, result, fields) {
+            if (err) throw err;
+            
+            res.send(result);
+        });
+
+});
+
+console.log(' RESTful API server started on: ' + port);
